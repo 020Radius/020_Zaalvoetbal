f9f6e3374070fec008b73a0ffe865d8f6096e7b4
koentjeh <koen-debont@hotmail.com>
2014-04-15 14:13:39 +0200

    resultaten.php

---
+++ Coderen/resultaten.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en">
+<head>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title>Fifa Def Edition</title>
+
+</head>
+<body>
+
+<div class="header">
+		<img class="headerlogo" src="img/ajax.png">
+	<div class="headertitel">
+		<h1>Fifa Dev Edition</h1>
+		<h2>Resultaten</h2>
+	</div>	
+</div>
+
+<div class="content">
+	<div class="teams">
+		<h3>Teams</h3>
+	</div>
+	<div class="poulestanden">
+		<h3>Poulestanden</h3>
+	</div>
+	<div class="wedstrijdschema">
+		<h3>Wedstrijdschema</h3>
+
+	</div>
+</div>
+
+<div class="footer">
+</div>
+
+</body>
+</html>
