7cc083d4dd74c1bce6423b1ee365699699bef0bd
jordynegen11 <jordy.visser@hotmail.com>
2014-04-22 13:36:04 +0200

    Includen config

--- Coderen/resultaten.php
+++ Coderen/resultaten.php
@@ -1,7 +1,6 @@
 <?php
-	session_start();
+require("./includes/config.php");
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html lang="en">
