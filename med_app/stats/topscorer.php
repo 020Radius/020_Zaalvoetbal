d374af552c4e8024da8d338fdc81161f061ce90d
jordynegen11 <jordy.visser@hotmail.com>
2014-05-12 10:14:23 +0200

    Topscoorders 5

--- med_app/stats/topscorer.php
+++ med_app/stats/topscorer.php
@@ -1,15 +1,17 @@
 <?php 
 require '../config/connect.php';
 
-$sql = "SELECT voornaam, tussenvoegsel, achternaam, doelpunten FROM spelers ORDER BY doelpunten DESC LIMIT 1";
+$sql = "SELECT voornaam, tussenvoegsel, achternaam, doelpunten FROM spelers ORDER BY doelpunten DESC LIMIT 5";
 $query = $mysqli->query($sql);
 
-if (mysqli_num_rows($query) == 1) {
-    $row = mysqli_fetch_assoc($query);
-    echo '<h2>Topscorer</h2>';
+if (mysqli_num_rows($query) == 5) {
+  echo '<h2>Topscorer</h2>';
+    while($row = mysqli_fetch_assoc($query))
+	{
+  
     echo '<h3>' . $row['voornaam'] . ' ' . $row['tussenvoegsel'] . ' ' . $row['achternaam'];
     echo '<span class="badge">' . $row['doelpunten'] . '</span></h3>'; 
-        
+     }   
 mysqli_close($mysqli);
 }
 
