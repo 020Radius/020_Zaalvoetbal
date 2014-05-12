e6edf835fd6b509932fe489002ed780ed53aa1e3
jordynegen11 <jordy.visser@hotmail.com>
2014-05-12 12:17:15 +0200

    resetall

--- med_app/resetData.php
+++ med_app/resetData.php
@@ -8,7 +8,7 @@ if ( !isset($_GET['msg']) == 'reset' )
 	die('Permission error');
 }
 
-	$sql = "UPDATE teams SET gewonnen = '0', punten = '0', gelijk = '0'";
+	$sql = "UPDATE teams SET gewonnen = '0', punten = '0', totaal_punten = '0', gelijk = '0'";
 	$query = $mysqli->query($sql);
 
 	$sql = "UPDATE poulewedstrijden SET goals_slot_1 = '0', goals_slot_2 = '0', gelijk = '0', winnaar = ''";
